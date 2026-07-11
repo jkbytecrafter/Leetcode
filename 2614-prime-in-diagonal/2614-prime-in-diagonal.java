b625cb0fdf690ef6baa104125f42b5fd3ddc064a
Jatin Kumar <jatinkumar15002@gmail.com>
2026-07-11 23:33:43 +0530

    Time: 9 ms (11.01%), Space: 82.4 MB (78.58%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.java
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPrime(int num){
         if(num<2) return false;
-        for(int i=2;i*i<=num;i++){
+        if (num == 2 || num == 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for(int i=5;i*i<=num;i++){
             if(num%i==0) return false;
         }
         return true;

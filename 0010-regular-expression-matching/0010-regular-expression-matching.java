72393b037985b1684fbfe550bd988a8f61a86cc3
Jatin Kumar <jatinkumar15002@gmail.com>
2026-08-10 03:43:30 +0530

    Time: 1 ms (99.99%), Space: 43.6 MB (34.13%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,21 +1,29 @@
 class Solution {
-    public boolean solve(String s, String p) {
-        if (p.length() == 0) {
-            return s.length() == 0;
-        }
-
-        boolean first_char_matched = s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
-
-        if (p.length() >= 2 && p.charAt(1) == '*') {
-            boolean not_take = solve(s, p.substring(2));
-            boolean take = first_char_matched && solve(s.substring(1), p);
-            return not_take || take;
-        }
-
-        return first_char_matched && solve(s.substring(1), p.substring(1));
+    public boolean isMatch(String s, String p) {
+        Boolean[][] memo = new Boolean[s.length() + 1][p.length() + 1];
+        return solve(0, 0, s, p, memo);
     }
 
-    public boolean isMatch(String s, String p) {
-        return solve(s, p);
+    private boolean solve(int i, int j, String s, String p, Boolean[][] memo) {
+        if (j == p.length()) {
+            return i == s.length();
+        }
+
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+
+        boolean firstCharMatched = (i < s.length() && 
+                                   (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+
+        boolean result;
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            result = solve(i, j + 2, s, p, memo) || 
+                    (firstCharMatched && solve(i + 1, j, s, p, memo));
+        } else {
+            result = firstCharMatched && solve(i + 1, j + 1, s, p, memo);
+        }
+
+        return memo[i][j] = result;
     }
 }

b65f42e89bebf0e5749faa417773ea90ee1e2e8c
Jatin Kumar <jatinkumar15002@gmail.com>
2026-08-23 15:52:22 +0530

    Time: 9 ms (29.93%), Space: 47.1 MB (26.28%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean sumGame(String num) {
         int l=num.length();
-        int nq=0;
-        for(int i=0;i<l;i++){
-            if(num.charAt(i) == '?') nq++;
-        }
-        if(nq%2==1) return true;
-        String firstHalf=num.substring(0,(l/2));
-        String secondHalf=num.substring((l/2),l);
         int nql=0,nqr=0,lsum=0,rsum=0;
-        for(int i=0;i<firstHalf.length();i++){
-            if(firstHalf.charAt(i) == '?') {
-                nql++;
+        for(int i=0;i<l;i++){
+            if(i < l/2){
+                if(num.charAt(i) == '?') {
+                    nql++;
+                }
+                else{
+                    lsum+=(num.charAt(i)) - '0';
+                }
             }
             else{
-                lsum+=(firstHalf.charAt(i)) - '0';
+                if(num.charAt(i) == '?') {
+                    nqr++;
+                }
+                else{
+                    rsum+=(num.charAt(i)) - '0';
+                }
             }
         }
-        for(int i=0;i<secondHalf.length();i++){
-            if(secondHalf.charAt(i) == '?') {
-                nqr++;
-            }
-            else{
-                rsum+=(secondHalf.charAt(i)) - '0';
-            }
-        }
+        if((nql+nqr)%2==1) return true;
         if( ((2*rsum)+(9*nqr)) == ((2*lsum)+(9*nql)) ) return false;
         return true;
     }
